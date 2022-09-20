1257508a56e175fb72a780d4a92fdcd8b8c254e4
Julian Cardone <juliancardone1@gmail.com>
2022-09-20 10:35:33 -0400

    completed the hangman project

--- hangman_project/hangman_project 2/lib/hangman.rb
+++ hangman_project/hangman_project 2/lib/hangman.rb
@@ -7,7 +7,7 @@ class Hangman
 
   def initialize
     @secret_word = Hangman.random_word
-    @guess_word = Array.new(@secret_word.length, "_")
+    @guess_word = Array.new(@secret_word.length){"_"}
     @attempted_chars = []
     @remaining_incorrect_guesses = 5
   end
@@ -34,7 +34,54 @@ def fill_indices(char, arr)
   end
 end
 
+def try_guess(char)
+  if !already_attempted?(char)
+    @attempted_chars << char 
+    if !get_matching_indices(char).empty?
+      fill_indices(char, get_matching_indices(char))
+    end
+    if !@secret_word.include?(char)
+      @remaining_incorrect_guesses -= 1
+    end
+    return true
+  end
+  if already_attempted?(char)
+    print "that has already been attempted" 
+    return false
+  end
+end
 
+def ask_user_for_guess
+  print "Enter a char"
+  char = gets.chomp
+  return try_guess(char)
+end
 
+def win?
+  if @guess_word.join('') == @secret_word
+    print 'WIN'
+    return true
+  else
+    return false
+  end
+end
+
+  def lose?
+    if @remaining_incorrect_guesses == 0
+      print 'LOSE'
+      return true
+    else
+      return false
+    end
+  end
+
+def game_over?
+  if win? || lose?
+    print @secret_word
+    return true
+  else
+    return false
+  end
+end
 
 end
