bb2c89106f45549596150a4c25d1fff32731c88c
Julian Cardone <juliancardone1@gmail.com>
2022-09-20 10:03:47 -0400

    finished part 1 of hangman

--- hangman_project/hangman_project 2/lib/hangman.rb
+++ hangman_project/hangman_project 2/lib/hangman.rb
@@ -1,4 +1,40 @@
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
 
+  def self.random_word
+    DICTIONARY.sample
+  end
+
+  def initialize
+    @secret_word = Hangman.random_word
+    @guess_word = Array.new(@secret_word.length, "_")
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
+  end
+
+  attr_reader :guess_word, :attempted_chars, :remaining_incorrect_guesses
+
+  def already_attempted?(char)
+    return true if @attempted_chars.include?(char)
+      false
+  end
+
+def get_matching_indices(char)
+  new_arr = []
+  @secret_word.each_char.with_index do |ele, i|
+    new_arr << i if ele == char
+  end
+  new_arr
+end
+
+def fill_indices(char, arr)
+  @guess_word.map!.with_index do |ele, i|
+    ele = char if arr.include?(i)
+    ele
+  end
+end
+
+
+
+
 end
