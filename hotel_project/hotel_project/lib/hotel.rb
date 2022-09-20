b8b28f7e883b947fe1739fefda7a30e3a3d345cc
Julian Cardone <juliancardone1@gmail.com>
2022-09-20 11:06:35 -0400

    finished room class

--- hotel_project/hotel_project/lib/hotel.rb
+++ hotel_project/hotel_project/lib/hotel.rb
@@ -1,5 +1,7 @@
 require_relative "room"
 
 class Hotel
-  
+
+
+
 end
--- hotel_project/hotel_project/lib/room.rb
+++ hotel_project/hotel_project/lib/room.rb
@@ -1,3 +1,30 @@
 class Room
+
+def initialize(capacity)
+    @capacity = capacity
+    @occupants = []
+end
+    
+attr_reader :capacity, :occupants
+
+def full?
+    return false if @occupants.length < @capacity
+    return true if @occupants.length == @capacity
+end
+
+def available_space
+    return @capacity - @occupants.length
+end
+
+def add_occupant(name)
+    if full?
+        return false
+    else
+        @occupants << name
+        return true
+    end
+end
+
+
   
 end
