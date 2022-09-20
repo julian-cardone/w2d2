a827772a638ea93e6f9784ddefa211b2b9f50940
Julian Cardone <juliancardone1@gmail.com>
2022-09-20 15:09:58 -0400

    finished part 2 startup class

--- startup_project/lib/startup.rb
+++ startup_project/lib/startup.rb
@@ -2,4 +2,53 @@ require "employee"
 
 class Startup
 
+    def initialize(name, funding, salaries)
+        @name = name
+        @funding = funding
+        @salaries = salaries
+        @employees = []
+    end
+
+    attr_reader :name, :funding, :employees, :salaries
+
+    def valid_title?(title)
+        if @salaries.has_key?(title)
+            return true
+        else
+            return false
+        end
+    end
+
+    def >(startup)
+        if @funding > startup.funding
+            return true
+        else
+            return false
+        end
+    end
+
+    def hire(name, title)
+        unless @salaries.has_key?(title)
+            raise ArgumentError.new "title is invalid"
+        end
+        @employees << Employee.new(name, title)
+    end
+
+    def size
+        return @employees.length
+    end
+
+    def pay_employee(emp_instance)
+        title = emp_instance.title
+        unless @funding >= salaries[title]
+            raise ArgumentError.new "ya broke"
+        end
+        emp_instance.pay(salaries[title])
+        @funding -= salaries[title]
+    end
+
+    def payday
+        employees.each {|person| pay_employee(person)}
+    end
+
 end
